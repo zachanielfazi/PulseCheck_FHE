d4ec37c6679d7cf21a486e9215f87aa66fed6ea7
zachanielfazi <zachanielfazi@users.noreply.github.com>
2025-11-13 16:16:37 +0000

    Add adapter-template.js, api-api-dynamic.css, router.tsx, secure-endpoint.scss

---
+++ model/serializer-manager-customer/adapter-template.js
@@ -0,0 +1,4 @@
+export const config = { runtime: 'edge' };
+
+interface fixtureProps { data: string }
+
---
+++ model/serializer-manager-customer/router.tsx
@@ -0,0 +1,4 @@
+export const config = { runtime: 'edge' };
+
+interface static_managerProps { data: string }
+
