b8ec49e789b5cbfad0e3ff57f864dc49e5bddb96
zachanielfazi <zachanielfazi@users.noreply.github.com>
2025-11-13 10:51:49 +0000

    Add queries-local-queries.jsx, router-queries-analytics.md, styles-profile-upload.ts, window.d.ts

---
+++ frontend/web/src/queries-local-queries.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+
+interface notificationProps { data: string }
+
---
+++ frontend/web/src/styles-profile-upload.ts
@@ -0,0 +1,4 @@
+import React from 'react';
+
+export const usedecorator_helpers = () => { return {}; };
+
